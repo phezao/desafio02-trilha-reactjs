720933f16bc717dc56989d821800d49d635e7d12
Philip Oka <philip.oka@gmail.com>
2021-09-16 12:40:24 -0300

    componentizando elementos dentro do app para tornar o código mais fácil de ser lido

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,27 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard"
+import { MovieProps } from "../App";
+import { GenreResponseProps } from "../App";
+
+interface ContentProps {
+  movies: MovieProps[],
+  selectedGenre: GenreResponseProps
+}
+
+
+export const Content = ({movies, selectedGenre}: ContentProps) => {
+  return(
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button"
+import { GenreResponseProps } from "../App";
+
+
+interface SideBarProps {
+  genres: GenreResponseProps[],
+  selectedGenreId: number,
+  setSelectedGenreId: (id: number) => void
+}
+
+export const SideBar = ({genres, selectedGenreId, setSelectedGenreId}: SideBarProps) => {
+
+  const handleClickButton = (id: number) => {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
